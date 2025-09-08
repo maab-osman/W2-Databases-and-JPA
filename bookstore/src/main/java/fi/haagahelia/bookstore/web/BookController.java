d5398e94e0011044492a2084c5bfa9597225916b
Maab Eltayeb <maabosman.edu@gmail.com>
2025-09-08 15:43:16 +0300

    Task 3

--- bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import fi.haagahelia.bookstore.model.Book;
 import fi.haagahelia.bookstore.repository.BookRepository;
+
 
 @Controller
 public class BookController {
@@ -18,4 +22,23 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/savebook")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+    
 }
