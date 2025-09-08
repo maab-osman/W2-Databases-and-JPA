2a0341fbef9c70a0fb0a7aa73994dd1ca0be9ef3
Maab Eltayeb <maabosman.edu@gmail.com>
2025-09-08 15:34:32 +0300

    Task 2

--- bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,5 +1,21 @@
 package fi.haagahelia.bookstore.web;
 
-public class BookController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import fi.haagahelia.bookstore.repository.BookRepository;
+
+@Controller
+public class BookController {
+    
+    @Autowired
+    private BookRepository repository;
+    
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
 }
